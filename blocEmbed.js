865f3757574bc42116501e1f10618f93630dbb13
Anmol Raj Soni <anmolrajsoni15@gmail.com>
2023-07-25 00:26:34 +0530

    Finally completed the task

--- blocEmbed.js
+++ blocEmbed.js
@@ -1,3 +1,5 @@
+//Copyright © 2023 Bloc. All rights reserved.
+
 var OPENED = false;
 
 function embedBlocChatBot() {
@@ -23,7 +25,9 @@ function embedBlocChatBot() {
   const popUpMessage = document.createElement("div");
   popUpMessage.setAttribute("id", "pop-up-message");
   popUpMessage.style.position = "fixed";
-  popUpMessage.style.bottom = "80px";
+  popUpMessage.style.bottom = "67px";
+  popUpMessage.style.right = "62px";
+  popUpMessage.style.maxWidth = "268px"
   popUpMessage.style.borderRadius = "10px";
   popUpMessage.style.fontFamily = "Arial, Helvetica, sans-serif";
   popUpMessage.style.fontSize = "14px";
@@ -32,18 +36,21 @@ function embedBlocChatBot() {
   popUpMessage.style.flexDirection = "column";
   popUpMessage.style.gap = "30px";
   popUpMessage.style.display = "none";
-  popUpMessage.innerHTML = `Hey there! How can i help you?  I am a AI Chatbot Powered By Bloc.`
+  popUpMessage.style.rotate = "10deg";
+  popUpMessage.style.scale = "0.1";
+  popUpMessage.style.transition = "all 0.3s ease-in-out";
+  popUpMessage.style.transformOrigin = "bottom right";
 
   const popUpMessageClose = document.createElement("div");
   popUpMessageClose.innerHTML = "&#10005;";
   popUpMessageClose.style.position = "absolute";
-  popUpMessageClose.style.top = "-7px";
-  popUpMessageClose.style.right = "-7px";
+  popUpMessageClose.style.top = "-3px";
+  popUpMessageClose.style.right = "-3px";
   popUpMessageClose.style.fontWeight = "bold";
   popUpMessageClose.style.display = "none";
   popUpMessageClose.style.justifyContent = "center";
   popUpMessageClose.style.alignItems = "center";
-  popUpMessageClose.style.zIndex = 999999996;
+  popUpMessageClose.style.zIndex = 999999998;
   popUpMessageClose.style.width = "22px";
   popUpMessageClose.style.height = "22px";
   popUpMessageClose.style.borderRadius = "50%";
@@ -96,7 +103,7 @@ function embedBlocChatBot() {
     });
 
     const iframe = document.createElement("iframe");
-    iframe.src = `https://embed.askbloc.ai/bloc/${blocId}`;
+    iframe.src = `http://embed.askbloc.ai/bloc/${blocId}`;
     iframe.setAttribute("id", "bloc-iframe");
     iframe.style.border = "none";
     iframe.style.position = "fixed";
@@ -105,7 +112,7 @@ function embedBlocChatBot() {
     iframe.style.boxShadow = "rgba(150, 150, 150, 0.2) 0px 10px 30px 0px, rgba(150, 150, 150, 0.2) 0px 0px 0px 1px";
     iframe.style.bottom = "80px";
   iframe.style.right = "1rem";
-  iframe.style.width = window.innerWidth < 640 ? "90%" : "458px";
+  iframe.style.width = window.innerWidth < 640 ? "90%" : "335px";
   iframe.style.height = "85vh";
   iframe.style.borderRadius = "0.75rem";
   iframe.style.display = "none";
@@ -115,6 +122,68 @@ function embedBlocChatBot() {
 
   document.body.appendChild(iframe);
 
+  const getFloatingMessage = () => {
+    const theme = "light";
+    // console.log(receiveMessage());
+
+    const messageContainer = document.createElement("div");
+    messageContainer.style.display = "flex";
+    messageContainer.style.justifyContent = "flex-end";
+
+    const messageEle = document.createElement("div");
+    messageEle.style.backgroundColor = "rgb(102 102 102)";
+    messageEle.style.color = "#ffffff";
+    messageEle.style.boxShadow =
+        "rgba(150, 150, 150, 0.2) 0px 10px 30px 0px, rgba(150, 150, 150, 0.2) 0px 0px 0px 1px";
+    messageEle.style.borderTopLeftRadius = "8px";
+    messageEle.style.borderTopRightRadius = "8px";
+    messageEle.style.borderBottomLeftRadius = "8px";
+    messageEle.style.padding = "8px 13px";
+    messageEle.style.margin = "8px";
+    messageEle.style.fontSize = "14px";
+    messageEle.innerText = `Hey there! How can i help you? I am a AI Chatbot Powered By Bloc.`;
+    messageEle.style.opacity = 0;
+  
+    messageEle.style.transform = "scale(0.9)";
+    messageEle.style.transition =
+          "opacity 0.5s ease, transform 0.5s ease";
+
+    messageContainer.appendChild(messageEle);
+    popUpMessage.appendChild(messageContainer);
+
+    setTimeout(() => {
+        popUpMessage.style.display = "block";
+    }
+    , 4000);
+
+    setTimeout(() => {
+        popUpMessage.style.scale = "1";
+    }, 4100);
+    
+    setTimeout(() => {
+        popUpMessage.style.bottom = "48px";
+        popUpMessage.style.rotate = "0deg";
+        messageEle.style.opacity = 1;
+        messageEle.style.transform = "scale(1)";
+    }, 4400);
+
+    popUpMessageClose.style.backgroundColor = theme === "light" ? "#fff" : "#000";
+    popUpMessageClose.style.color = theme === "light" ? "#000" : "#fff";
+    popUpMessageClose.style.boxShadow = theme === "light" ? "rgba(150, 150, 150, 0.2) 0px 10px 30px 0px, rgba(150, 150, 150, 0.2) 0px 0px 0px 1px" : "rgba(0, 0, 0, 0.2) 0px 10px 30px 0px, rgba(0, 0, 0, 0.2) 0px 0px 0px 1px";
+
+    popUpMessage.addEventListener("mouseenter", () => {
+        popUpMessageClose.style.display = "flex";    
+    });
+
+    popUpMessage.addEventListener("mouseleave", () => {
+        popUpMessageClose.style.display = "none";
+    });
+
+    popUpMessageClose.addEventListener("click", () => {
+        popUpMessage.style.display = "none";
+    });
+  };
+
     function getBlocIcon() {
         const CHAT_BUTTON_ICON = `https://embed.askbloc.ai/bloc_logo.svg`;
         const imageEleOpen = `<img src="${CHAT_BUTTON_ICON}" width="45%" height="45%" />`
@@ -123,13 +192,14 @@ function embedBlocChatBot() {
       }
     
       function getCloseIcon() {
-        const CHAT_BUTTON_CLOSE = `https://icon2.cleanpng.com/20180203/paw/kisspng-menu-arrow-icon-down-arrow-png-transparent-image-5a756e457638b4.3161758215176453814842.jpg`;
+        const CHAT_BUTTON_CLOSE = `http://embed.askbloc.ai/down.svg`;
 
-        const imageEle = `<img src="${CHAT_BUTTON_CLOSE}" width="50%" height="50%" style={{mixBlendMode: "color-burn"}} />`
+        const imageEle = `<img src="${CHAT_BUTTON_CLOSE}" width="38%" height="38%" />`
         return imageEle;
       }
 
       document.body.appendChild(blocBot);
+      getFloatingMessage();
 
       iframe.onload = () => {
         iframe.contentWindow.postMessage(
